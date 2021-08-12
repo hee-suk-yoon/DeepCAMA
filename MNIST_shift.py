ed4d63e327468d48fe37028c1b09d176af512875
hskyoon <tomyoon2@gmail.com>
2021-08-13 07:05:37 +0900

    .

--- MNIST_shift.py
+++ MNIST_shift.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 def shift_imaged(x,y,width_shift_val,height_shift_val):
     #x is assumed to be a tensor of shape (#batch_size, #channels, width, height) = (#batch size, 1, 28, 28)
     #y is assumed to be a tensor of shape (#batch_size)
@@ -19,4 +20,16 @@ def shift_imaged(x,y,width_shift_val,height_shift_val):
     X = torch.from_numpy(X).view(batch_size,1,28,28)
     Y = torch.from_numpy(Y).view(batch_size)
 
+    return X,Y
+
+def shift_image_v2(x,y,width_shift_val,height_shift_Val):
+    batch_size = x.size()[0]
+    x = x.detach().cpu().numpy().reshape(batch_size,28,28)
+    y = y.detach().cpu().numpy().reshape(batch_size)
+    
+    shift_width_pixel = round(28*width_shift_val)
+    shift_height_pixel = round(28*height_shift_Val)
+    
+    X = np.roll(x,shift_height_pixel, axis = 1)
+    Y = y
     return X,Y
--- main.py
+++ main.py
@@ -58,6 +58,10 @@ def shift_image_v2(x,y,width_shift_val,height_shift_Val):
     batch_size = x.size()[0]
     x = x.detach().cpu().numpy().reshape(batch_size,28,28)
     y = y.detach().cpu().numpy().reshape(batch_size)
+    
+    shift_width_pixel = round(28*width_shift_val)
+    shift_height_pixel = round(28*height_shift_Val)
+    
     return
 #  ----------------------------------------------------
 
@@ -65,7 +69,7 @@ def shift_image_v2(x,y,width_shift_val,height_shift_Val):
 parser = argparse.ArgumentParser(description='DeepCAMA MNIST Example')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=80, metavar='N',
+parser.add_argument('--epochs', type=int, default=600, metavar='N',
                     help='number of epochs to train (default: 10)')
 parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
@@ -189,7 +193,7 @@ class DeepCAMA(nn.Module):
 
 
 model = DeepCAMA().to(device)
-optimizer = optim.Adam(model.parameters(), lr=1e-4)
+optimizer = optim.Adam(model.parameters(), lr=(1e-4+1e-5)/2)
 
 # Reconstruction + KL divergence losses summed over all elements and batch
 def loss_function(recon_x, x, y, mu_q1, logvar_q1, mu_q2, logvar_q2):
@@ -268,7 +272,7 @@ def pred(x):
         #m = torch.zeros(x.size()[0], 32)
         #calculate the log-ration term for each y = c(as an approximation to log p(x,y=c))
         sum = 0
-        K = 100
+        K = 200
         for j in range(0,K):
             sum = sum + pred_logRatio(x.to(device), x_recon.to(device), mu_q1.to(device), logvar_q1.to(device), m.to(device))
         log_pxy = torch.log(sum).view(x.size()[0]).detach().cpu().numpy()
@@ -294,11 +298,11 @@ if __name__ == "__main__":
     #for epoch in range(1, args.epochs + 1):
     #    train(epoch)
     
-    #torch.save(model.state_dict(), '/media/hsy/DeepCAMA/weight.pt')
+    #torch.save(model.state_dict(), '/media/hsy/DeepCAMA/weight3.pt') #ephochs : 300, lr (1e-4+1e-5)/2, Loss:89
+    #torch.save(model.state_dict(), '/media/hsy/DeepCAMA/weight4.pt') #ephochs : 600    ""                   87.1662
     
-    
-    model.load_state_dict(torch.load('/media/hsy/DeepCAMA/weight.pt', map_location=device))
-    model.eval()
+    #model.load_state_dict(torch.load('/media/hsy/DeepCAMA/weight3.pt', map_location=device))
+    #model.eval()
 
     """
     a,y = next(iter(test_loader)) 
@@ -316,30 +320,43 @@ if __name__ == "__main__":
     #save_image(a[8].view(1,28,28),'actual.png')
     #save_image(x_recon[8].view(1,28,28),'temp1.png')
     """
-
+    
+    
+    
+    
+    
+    """
     temp = 0
     total_i = 0 
     vertical_shift_range = np.arange(start=0.0,stop=1.0,step=0.1)
+    print(vertical_shift_range)
     accuracy_list = [0]*vertical_shift_range.shape[0]
     index = 0
     for vsr in vertical_shift_range:
-        for i, (data, y) in enumerate(test_loader):
-            if (data.size()[0] == args.batch_size): #resolve last batch issue later.
-                data, y = shift_image(x=data,y=y,width_shift_val=0.0,height_shift_val=vsr)
-                y_pred = pred(data)
-                y_temp = y.detach().cpu().numpy()
-                aa = accuracy(y_temp,y_pred)
-                temp = temp + aa
-                total_i = total_i + 1
-                #print(aa)
-        print(temp/total_i)
-        accuracy_list[index] = temp/total_i
-        index = index + 1 
-        #print(temp/total_i)
-    print(accuracy)
+        if (vsr <= 0.21 and vsr >= 0.19):
+            print('here')
+            for i, (data, y) in enumerate(test_loader):
+                if (data.size()[0] == args.batch_size): #resolve last batch issue later.
+                    data, y = shift_image(x=data,y=y,width_shift_val=0.0,height_shift_val=vsr)
+                    y_pred = pred(data)
+                    y_temp = y.detach().cpu().numpy()
+                    aa = accuracy(y_temp,y_pred)
+                    temp = temp + aa
+                    total_i = total_i + 1
+                    #print(aa)
+            print(temp/total_i)
+            accuracy_list[index] = temp/total_i
+            index = index + 1 
+            #print(temp/total_i)
+    #print(accuracy)
     
     plt.plot(vertical_shift_range,accuracy_list)
     plt.show()
+    """
+    
+    
+    
+    
     """
     x_recon, mu_q1, logvar_q1, mu_q2, logvar_q2 = model(a.to(device),y.to(device), manipulated=False)
     save_image(x_recon[0].view(1,28,28),'temp2.png')
